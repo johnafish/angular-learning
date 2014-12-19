d1aa4c5c965472f03808c4f57bb8d3b076b139e2
John Fish <john@johnafish.ca>
2014-12-18 20:20:20 -0500

    Added support for multiple products

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 (function(){
 	var app = angular.module("store", []);
 	app.controller("StoreController", function(){
-		this.product = gem;
+		this.products = gems;
 	});
-	var gem = {
+	var gems = [
+	{
 		name: "Triangle",
 		price: 3.52,
+		canPurchase: true,
+		soldOut: false,
 		description: "My interior angles add up to 180 degrees and if that's not enough for you I don't know what is."
+	},
+	{
+		name: "Circle",
+		price: 9.99,
+		canPurchase: true,
+		soldOut: false,
+		description: "I'm actually a perfect circle. Seriously. No, I'm not lying!"
 	}
+	]
 })();
