f8763c53bb4243e766ad2dc4d34e96c84ecf2db0
John Fish <john@johnafish.ca>
2014-12-18 19:48:37 -0500

    Added a controller and presented our first product

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var app = angular.module("Store", []);
+(function(){
+	var app = angular.module("store", []);
+	app.controller("StoreController", function(){
+		this.product = gem;
+	});
+	var gem = {
+		name: "Triangle",
+		price: 3.52,
+		description: "My interior angles add up to 180 degrees and if that's not enough for you I don't know what is."
+	}
+})();
